8f2f9b1fdfeefc8cbba6a4a10758c07dd846a582
Carl <carlparm@gmail.com>
2020-06-12 15:55:32 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li><a href="contact">Contact</a></li>
+        <li><a href="https://medium.com/@carlparm" target="_blank" rel="noopener noreferrer">Blog</a></li>
+        <li><a href="https://drive.google.com/file/d/1JKksq5y1iRpHEl-6xp8-SgPWRXIx8rej/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
+        <li><a href="contact.asp">Portfolio</a></li>
+        <li><a href="about.asp">About</a></li>
+        <li><b>Carl Parm</b></li>
+      </ul>
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
