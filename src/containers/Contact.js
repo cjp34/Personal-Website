fd881ecc013a2a92a895779f8a3f03e4f08397d5
Carl <carlparm@gmail.com>
2020-07-26 17:45:55 -0400

    contact icons

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -8,7 +8,13 @@ const Contact = () => {
 
     return(
         <div>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h2 class='sectionHeaders'>Contact</h2>
+            <div class='contactItems'>
+                <h3>Email: carlparm@gmail.com</h3>
+                <a href='https://www.linkedin.com/in/carlparm/' target='blank'><h3><i class="fa fa-linkedin-square"></i>LinkedIn</h3></a>
+                <a href='https://github.com/cjp34'><h3><i class="fa fa-github"></i>Github</h3></a>
+            </div>
         </div>
     )
 }
